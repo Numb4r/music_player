db76d5100aaa90bfc7a96548335ec18fb8496f0a
Numb4r <yuri.d.d.faria@gmail.com>
2023-02-23 00:43:34 -0300

    reescrito o main para adaptive theme mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,51 @@
+import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:flutter/material.dart';
-import 'package:media_player/screens/home.dart';
 
-void main(List<String> args) {
-  runApp(const MaterialApp(
-    home: Home(),
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:media_player/models/profile.dart';
+import 'package:media_player/screens/config_screen.dart';
+
+import 'package:media_player/screens/folder_configuration_screen.dart';
+import 'package:media_player/screens/home_screen.dart';
+import 'package:provider/provider.dart';
+
+Future<void> main(List<String> args) async {
+  await Hive.initFlutter();
+
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(
+        create: (_) => Profile(),
+      ),
+    ],
+    child: const MyApp(),
   ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AdaptiveTheme(
+      light: ThemeData(
+        brightness: Brightness.light,
+        // colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.red)
+        //     .copyWith(secondary: Colors.amber),
+      ),
+      dark: ThemeData(
+        brightness: Brightness.dark,
+        // colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.red)
+        //     .copyWith(secondary: Colors.amber),
+      ),
+      initial: context.read<Profile>().isDarkMode
+          ? AdaptiveThemeMode.dark
+          : AdaptiveThemeMode.light,
+      builder: (theme, darkTheme) => MaterialApp(
+        theme: theme,
+        darkTheme: darkTheme,
+        home: const FolderConfigurationScreen(),
+      ),
+    );
+  }
 }
